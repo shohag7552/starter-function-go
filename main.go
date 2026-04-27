eb69b855ab23d0ab336c8d9f9f66484c7461a1d5
Mehedi05739 <mehedi05739@gmail.com>
2026-04-28 00:16:19 +0600

    provider id for manage specific deliverymans

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ type Payload struct {
 	Title   string `json:"title"`
 	Message string `json:"message"`
 	OrderId string `json:"orderId"` // Optional, for order tracking
+	ProviderId string `json:"providerId"` // Optional, for provider tracking
 }
 
 func Main(Context openruntimes.Context) openruntimes.Response {
@@ -80,6 +81,7 @@ func Main(Context openruntimes.Context) openruntimes.Response {
 			messaging.WithCreatePushTitle(payload.Title),
 			messaging.WithCreatePushBody(payload.Message),
 			messaging.WithCreatePushUsers([]string{payload.UserId}),
+			messaging.WithCreatePushProviders([]string{payload.ProviderId}),
 			messaging.WithCreatePushData(data),
 		)
 
@@ -96,55 +98,3 @@ func Main(Context openruntimes.Context) openruntimes.Response {
 		"message": "Notification processed successfully",
 	})
 }
-
-// package handler
-
-// import (
-// 	"os"
-// 	"strconv"
-
-// 	"github.com/appwrite/sdk-for-go/appwrite"
-// 	"github.com/open-runtimes/types-for-go/v4/openruntimes"
-// )
-
-// type Response struct {
-// 	Motto       string `json:"motto"`
-// 	Learn       string `json:"learn"`
-// 	Connect     string `json:"connect"`
-// 	GetInspired string `json:"getInspired"`
-// }
-
-// // This Appwrite function will be executed every time your function is triggered
-// func Main(Context openruntimes.Context) openruntimes.Response {
-// 	// You can use the Appwrite SDK to interact with other services
-// 	// For this example, we're using the Users service
-// 	client := appwrite.NewClient(
-// 		appwrite.WithEndpoint(os.Getenv("APPWRITE_FUNCTION_API_ENDPOINT")),
-// 		appwrite.WithProject(os.Getenv("APPWRITE_FUNCTION_PROJECT_ID")),
-// 		appwrite.WithKey(Context.Req.Headers["x-appwrite-key"]),
-// 	)
-// 	users := appwrite.NewUsers(client)
-
-// 	response, err := users.List()
-// 	if err != nil {
-// 		Context.Error("Could not list users: " + err.Error())
-// 	} else {
-// 		// Log messages and errors to the Appwrite Console
-// 		// These logs won't be seen by your end users
-// 		Context.Log("Total users: " + strconv.Itoa(response.Total))
-// 	}
-
-// 	// The req object contains the request data
-// 	if Context.Req.Path == "/ping" {
-// 		// Use res object to respond with text(), json(), or binary()
-// 		// Don't forget to return a response!
-// 		return Context.Res.Text("Pong")
-// 	}
-
-// 	return Context.Res.Json(Response{
-// 		Motto:       "Build like a team of hundreds_",
-// 		Learn:       "https://appwrite.io/docs",
-// 		Connect:     "https://appwrite.io/discord",
-// 		GetInspired: "https://builtwith.appwrite.io",
-// 	})
-// }
